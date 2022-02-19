6d343690879baf54614d3663ebf1b7d60d79167d
Ágata Ordano <ordanoagata@gmail.com>
2022-02-19 17:43:06 -0300

    ci(test): forgot to add puppeteer
    it seems doing CI with headless browsers is not as straightforward as my initiali naiveté assumed

--- karma.config.js
+++ karma.config.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Mon Feb 14 2022 16:28:20 GMT-0300 (Argentina Standard Time)
 
+process.env.CHROME_BIN = require("puppeteer").executablePath()
+
 module.exports = function (config) {
 	config.set({
 		// base path that will be used to resolve all patterns (eg. files, exclude)
